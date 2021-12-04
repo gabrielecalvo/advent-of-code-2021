d43381ebb69add342c7eab6d2e3855196128f0ef
Gabe Calvo <gcalvo87@gmail.com>
2021-12-04 11:39:05 +0000

    day 4

--- solutions.py
+++ solutions.py
@@ -14,7 +14,7 @@
 
 from utils import get_input
 
-# + [markdown] jp-MarkdownHeadingCollapsed=true tags=[]
+# + [markdown] jp-MarkdownHeadingCollapsed=true tags=[] jp-MarkdownHeadingCollapsed=true jp-MarkdownHeadingCollapsed=true tags=[]
 # # Day 1
 # -
 
@@ -50,7 +50,7 @@ for value in triplet_sum:
 
 print(f"Solution is: {increases}")
 
-# + [markdown] jp-MarkdownHeadingCollapsed=true tags=[]
+# + [markdown] jp-MarkdownHeadingCollapsed=true tags=[] jp-MarkdownHeadingCollapsed=true jp-MarkdownHeadingCollapsed=true tags=[]
 # # Day 2
 # -
 
@@ -106,9 +106,10 @@ for row in content.split("\n"):
     aim += daimd
     
 print(f"Solution {x*y}")
-# -
 
+# + [markdown] tags=[] jp-MarkdownHeadingCollapsed=true jp-MarkdownHeadingCollapsed=true tags=[]
 # # Day 3
+# -
 
 content = get_input("day03.txt")
 diagnostics = [i for i in content.split("\n") if i]
@@ -175,5 +176,100 @@ for i in range(n):
 
 product = int(oxigen_value,2) * int(co2_value,2)
 print(f"Solution {product}")
+
+# + [markdown] tags=[]
+# # Day 4
+# -
+
+content = get_input("day04.txt")
+content_rows = content.split("\n")
+
+# +
+from itertools import product
+
+rowcol = [(r,c) for r,c in product(range(5), range(5))]
+        
+class Board:
+    def __init__(self, rows, name):
+        self.name = name
+        self.values = [[int(i) for i in r.split()] for r in rows]
+        self.mask = [[0 for i in range(5)] for j in range(5)]
+        
+    def __repr__(self):
+        res = ""
+        for row, col in rowcol:
+            value = self.values[row][col]
+            mask = self.mask[row][col]
+            prefix = "\n" if col == 0 else ""
+            res += f"{prefix}" + (f"({value:>2})" if mask else f" {value:>2} ")
+        return res
+    
+    def tick_number(self, n):
+        for row, col in rowcol:
+            if self.values[row][col] == n:
+                self.mask[row][col] = 1
+                return True
+        return False
+    
+    def check_board(self):
+        for r in self.mask:
+            if sum(r) == 5:
+                return True
+            
+        for c in range(5):
+            if sum(r[c] for r in self.mask) == 5:
+                return True
+            
+        return None
+        
+    def sum_unmasked(self):
+        unmarked_sum = 0
+        for row, col in rowcol:
+            if self.mask[row][col] == 0:
+                unmarked_sum += self.values[row][col]
+            
+        return unmarked_sum
+    
+def build_boards(content_rows):
+    boards = {}
+    for i in range(2, len(content_rows), 6):
+        boards[i] = Board(content_rows[i:i+5], name=i)
+    return boards
+
+
+# -
+
+# ## part 1
+
+def find_solution(extracted_numbers, boards):
+    for n in extracted_numbers:
+        for board in boards.values():
+            if board.tick_number(n):
+                if board.check_board():
+                    return board.sum_unmasked() * n
+
+
+extracted_numbers = [int(i) for i in content_rows[0].split(",")]
+boards = build_boards(content_rows)
+
+find_solution(extracted_numbers, boards)
+
+
+# ## part 2
+
+# +
+def find_solution(extracted_numbers, boards):
+    remaining_boards = boards.copy()
+    for n in extracted_numbers:
+        for board_name, board in boards.items():
+            if board_name not in remaining_boards: continue
+            if not board.tick_number(n): continue
+            if not board.check_board(): continue
+            remaining_boards.pop(board_name)
+            if len(remaining_boards) == 0:
+                return board.sum_unmasked() * n
+
+find_solution(extracted_numbers, boards)
+# -
 
 
